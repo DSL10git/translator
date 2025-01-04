256133f45bc752bd77866e407ed0c5adb1cb786a
Daniel sun Liao <danielsunliao@gmail.com>
2025-01-04 13:49:27 -0800

    Ignore more files

--- commands.sh
+++
@@ -1,7 +0,0 @@
-REMOTE_SERVER=ubuntu@ec2-54-188-57-227.us-west-2.compute.amazonaws.com
-KEY_FILE=/Users/dandan/.ssh/test.pem
-
-# Copy a file to remote server
-LOCAL_FILE=main.py
-REMOTE_LOCATION=/home/ubuntu/projects/chatbot
-scp -i $KEY_FILE $LOCAL_FILE $REMOTE_SERVER:$REMOTE_LOCATION
